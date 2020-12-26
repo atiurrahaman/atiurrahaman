2741ba2ec94edba9a41f65e2b77446758e2ce7f6
Atiurrahaman <rahamanat65@gmail.com>
2020-12-26 20:36:35 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function hello() {
+    alert("This button is empty till now")
+}
+
+function hello2() {
+    alert("You have removed focus from hello")
+}
+// You have your mouse on hello
+function hello3() {
+    
+}
+
+function hello4() {
+    alert("You have removed your mouse from hello")
+}
+
+function hello5() {
+    alert("you have copied text")
+}
+
